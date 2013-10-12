fb0369d651478acc6c32a58b3ce31cf7c4967bf1
Grag Atrim <github@gragatrim.com>
2013-10-12 16:43:26 -0400

    master commit 1
    started using/passing in destination branch/remote
    
    created a hacky fix to test the command out
    
    Added some comments and escaped some args
    
    deleting a bunch of test files

---
+++ butlertron.php
@@ -0,0 +1,21 @@
+<?php
+//Get all of the command line arguments
+//b is the comma separated list of branches that you would like to merge into the destination branch
+//d is the destination branch you would like the branches merged into
+//r is the remote repository of the destination branch
+$options = getopt("b:d:r::");
+if (empty($options['r'])) {
+  $remote_tracking_branch = '';
+  $remote_tracking_branch_command = '';
+} else {
+  $remote_repository = $options['r'];
+  $remote_repository = escapeshellarg($options['r'] . '/');
+}
+$destination_branch = escapeshellarg($options['d']);
+$branches = explode(",", $options['b']);
+foreach ($branches AS $branch) {
+  $clean_branch = escapeshellarg($branch);
+  $git_merge_command = 'git checkout ' . $clean_branch . ' && git merge --no-edit ' . $remote_repository . $destination_branch .
+                       ' && git checkout ' . $remote_repository . $destination_branch . ' && git merge --no-edit ' . $clean_branch;
+  exec($git_merge_command, $ouput, $error);
+}
