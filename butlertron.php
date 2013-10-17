659d15d9d837cdfae3e8f0085824e9d395505955
Grag Atrim <github@gragatrim.com>
2013-10-17 19:09:36 -0400

    Fixed a typo, and set the correct amount of colons for the command line argument
    trying to checkout a branch with the remote repo in it was a silly
    idea...

--- butlertron.php
+++ butlertron.php
@@ -3,19 +3,19 @@
 //b is the comma separated list of branches that you would like to merge into the destination branch
 //d is the destination branch you would like the branches merged into
 //r is the remote repository of the destination branch
-$options = getopt("b:d:r::");
+$options = getopt("b:d:r:");
 if (empty($options['r'])) {
-  $remote_tracking_branch = '';
-  $remote_tracking_branch_command = '';
+  $remote_repository = '';
+  $remote_repository_command = '';
 } else {
   $remote_repository = $options['r'];
-  $remote_repository = escapeshellarg($options['r'] . '/');
+  $remote_repository_command = escapeshellarg($options['r'] . '/');
 }
 $destination_branch = escapeshellarg($options['d']);
 $branches = explode(",", $options['b']);
 foreach ($branches AS $branch) {
   $clean_branch = escapeshellarg($branch);
-  $git_merge_command = 'git checkout ' . $clean_branch . ' && git merge --no-edit ' . $remote_repository . $destination_branch .
-                       ' && git checkout ' . $remote_repository . $destination_branch . ' && git merge --no-edit ' . $clean_branch;
+  $git_merge_command = 'git checkout ' . $clean_branch . ' && git merge --no-edit ' . $remote_repository_command . $destination_branch .
+                       ' && git checkout ' . $destination_branch . ' && git merge --no-edit ' . $clean_branch;
   exec($git_merge_command, $ouput, $error);
 }
